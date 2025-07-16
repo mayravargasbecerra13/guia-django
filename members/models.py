2a5a17e5f84d3d6d05ab15b24071f1c584eb1b19
mayravargasbecerra13 <vargasmayra800@gmail.com>
2025-07-16 12:23:18 -0500

    Agregue campos y datos a la base de datos

--- members/models.py
+++ members/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+    gender = models.CharField(max_length=255, null=True)
+    birthay = models.DateField(null=True)
+    email = models.EmailField(null=True)
